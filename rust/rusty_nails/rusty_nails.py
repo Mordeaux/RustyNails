d199d17a8749757f338fed27128c53037fa4041b
Michael T. Mordowanec <michael.mordowanec@gmail.com>
2015-08-28 14:40:46 -0400

    stupid python changes

--- rust/rusty_nails/rusty_nails.py
+++ rust/rusty_nails/rusty_nails.py
@@ -1,12 +1,19 @@
+import os
 from ctypes import cdll
 from sys import platform
+
+# Get the absolute path of the current directory so this file can be
+# called from outside its home directory.
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 if platform == "darwin":
     ext = "dylib"
 else:
     ext = "so"
 
-lib = cdll.LoadLibrary('target/release/librusty_nails.' + ext)
+rust_lib_path = os.path.join(DIRECTORY, 'target/release/librusty_nails.{}').format(ext)
+
+lib = cdll.LoadLibrary(rust_lib_path)
 lib.process()
 
 print("Python is done!")
