61039c4045b9efa646d17f93ae649e95c3387fe6
Michael T. Mordowanec <michael.mordowanec@gmail.com>
2015-08-29 14:19:49 -0400

    got python multithreading

--- python/python_compare.py
+++ python/python_compare.py
@@ -14,18 +14,18 @@ def single_thread():
 def multithread():
     def worker():
         count = 0
-        for i in xrange(50000000): 
+        for i in xrange(5000000): 
             count += 1
         print "thread finished at count: {}".format(count)
 
-    jobs = []
+    threads = []
     for i in xrange(10):
         thread = Process(target=worker)
-        jobs.append(thread)
+        threads.append(thread)
         thread.start()
 
-    for job in jobs:
-        job.join()
+    for thread in threads:
+        thread.join()
 
     print "python is done"
 
